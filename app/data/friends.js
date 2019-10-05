c212984d0941332d01feb5300ac8dbaab6c2b2a1
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-04 18:31:18 -0700

    add more default friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friends = [
         name: "Corgi",
         photo: "https://www.rover.com/blog/wp-content/uploads/2019/01/6342530545_45ec8696c8_b-960x540.jpg",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Husky",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Husky_on_San_Francisco_sidewalk.jpg/220px-Husky_on_San_Francisco_sidewalk.jpg",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Beagle",
+        photo: "https://i.ytimg.com/vi/bx7BjjqHf2U/maxresdefault.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ]
 module.exports = friends;
