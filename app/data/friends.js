282f65f5067341e672d5fa0460f7cf173e50f21b
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-04 18:19:52 -0700

    working that apiroute...

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
     {
         name: "Corgi",
         photo: "https://www.rover.com/blog/wp-content/uploads/2019/01/6342530545_45ec8696c8_b-960x540.jpg",
-        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     }
 ]
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,25 @@
 var friends = require("../data/friends.js");
 
 // export them!
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         return res.json(friends)
     });
 
-    app.post("/api/friends", function(req, res) {
-        // do something when posted info comes in
+    app.post("/api/friends", function (req, res) {
+        // make a friend object!
+        var friend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: req.body.scores
+        }
+
+        // add friend to databases!
+        friends.push(friend)
+
+        // do some magic to find the best matching friend!
+        var score = req.body.scores;
+
     });
 }
