4c76da214ee5e39f300702a352a924dd706d607e
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-04 18:16:27 -0700

    add more logic and a default friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,8 @@
-var friends = []
+var friends = [
+    {
+        name: "Corgi",
+        photo: "https://www.rover.com/blog/wp-content/uploads/2019/01/6342530545_45ec8696c8_b-960x540.jpg",
+        score: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+]
 module.exports = friends;
