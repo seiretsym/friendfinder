f8d784f32f4b505d77c06dad860d863d112aa924
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-04 18:31:36 -0700

    add friend matching logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,11 +16,33 @@ module.exports = function (app) {
             scores: req.body.scores
         }
 
-        // add friend to databases!
-        friends.push(friend)
-
-        // do some magic to find the best matching friend!
+        // do some magic to find the best food friend
         var score = req.body.scores;
 
+        // create a match score
+        var match = 40; // because 5-1 = 4 and 10*4 = 40, so the worst friend has a highest of 40
+        var friendIndex = 0;
+
+        // compare each friend...
+        friends.forEach(function(bff, index) {
+            var diff = 0;
+            // loop through scores...
+            for (var i = 0; i < 10; i++) {
+                // calculate diff by comparing scores
+                diff += Math.abs(bff.scores[i] - score[i]);
+            }
+            // if this friend is better...
+            if (diff < match) {
+                // change match score
+                match = diff;
+                // also save the friend's index for later
+                friendIndex = index;
+            }
+        })
+        // send the bff's info!
+        res.send(friends[friendIndex]);
+
+        // add friend to databases!
+        friends.push(friend)
     });
 }
